fa806a5d15160f97d04dece86ed81c1ffded6dcb
MahirPatel2905 <Mahirpatel222905@gmail.com>
2025-01-11 14:01:02 +0530

    Update Q3.cpp

--- Constructor,Destructor,Encapsulation & Abstraction/Q3.cpp
+++ Constructor,Destructor,Encapsulation & Abstraction/Q3.cpp
@@ -1,3 +1,47 @@
 /* Write a C++ program to create a class called Car that has private
 member variables for company, model, and year. Implement member
 functions to get and set these variables.*/ 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car {
+private:
+    string company;
+    string model;
+    int year;
+
+public:
+    void setDetails(string c, string m, int y) {
+        company = c;
+        model = m;
+        year = y;
+    }
+
+    void getDetails() {
+        cout << "Company: " << company << endl;
+        cout << "Model: " << model << endl;
+        cout << "Year: " << year << endl;
+    }
+};
+
+int main() {
+    Car car;
+    string company, model;
+    int year;
+
+    cout << "Enter car company: ";
+    cin >> company;
+    cout << "Enter car model: ";
+    cin >> model;
+    cout << "Enter car year: ";
+    cin >> year;
+
+    car.setDetails(company, model, year);
+    cout << "\nCar Details:" << endl;
+    car.getDetails();
+
+    return 0;
+}
+
