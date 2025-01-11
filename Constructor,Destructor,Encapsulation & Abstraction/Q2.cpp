8ab081c8791d16e661b0eb8a0513e5bce3b14ade
MahirPatel2905 <Mahirpatel222905@gmail.com>
2025-01-11 13:59:34 +0530

    Update Q2.cpp

--- Constructor,Destructor,Encapsulation & Abstraction/Q2.cpp
+++ Constructor,Destructor,Encapsulation & Abstraction/Q2.cpp
@@ -1,1 +1,48 @@
 // Write a program of Addition, Subtraction, Division, Multiplication using constructor.
+
+#include <iostream>
+using namespace std;
+
+class Calculator {
+    int num1, num2;
+
+public:
+    Calculator(int a, int b) {
+        num1 = a;
+        num2 = b;
+    }
+
+    void add() {
+        cout << "Addition: " << (num1 + num2) << endl;
+    }
+
+    void subtract() {
+        cout << "Subtraction: " << (num1 - num2) << endl;
+    }
+
+    void multiply() {
+        cout << "Multiplication: " << (num1 * num2) << endl;
+    }
+
+    void divide() {
+        if (num2 != 0)
+            cout << "Division: " << (num1 / (float)num2) << endl;
+        else
+            cout << "Division by zero is not allowed." << endl;
+    }
+};
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    Calculator calc(a, b);
+    calc.add();
+    calc.subtract();
+    calc.multiply();
+    calc.divide();
+
+    return 0;
+}
+
