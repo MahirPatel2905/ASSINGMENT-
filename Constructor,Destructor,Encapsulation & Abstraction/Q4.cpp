ebbc743112a9838e2886589a855514c5051a71da
MahirPatel2905 <Mahirpatel222905@gmail.com>
2025-01-11 14:04:01 +0530

    Update Q4.cpp

--- Constructor,Destructor,Encapsulation & Abstraction/Q4.cpp
+++ Constructor,Destructor,Encapsulation & Abstraction/Q4.cpp
@@ -2,3 +2,62 @@
 private member variables for account number and balance. Include
 member functions to deposit and withdraw money from the account. */
 
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+private:
+    int accountNumber;
+    double balance;
+
+public:
+    BankAccount(int accNo, double initialBalance) {
+        accountNumber = accNo;
+        balance = initialBalance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposited: " << amount << endl;
+    }
+
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            cout << "Withdrawn: " << amount << endl;
+        } else {
+            cout << "Insufficient balance!" << endl;
+        }
+    }
+
+    void display() {
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+
+int main() {
+    int accNo;
+    double initialBalance;
+
+    cout << "Enter account number: ";
+    cin >> accNo;
+    cout << "Enter initial balance: ";
+    cin >> initialBalance;
+
+    BankAccount account(accNo, initialBalance);
+
+    account.display();
+
+    account.deposit(5000);
+    account.display();
+
+    account.withdraw(2000);
+    account.display();
+
+    account.withdraw(10000); 
+
+    return 0;
+}
+
+
