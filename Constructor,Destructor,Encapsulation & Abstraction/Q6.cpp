0dc07d8041e3f05b60ed4d759325e0021ac49d5d
MahirPatel2905 <Mahirpatel222905@gmail.com>
2025-01-11 14:08:05 +0530

    Update Q6.cpp

--- Constructor,Destructor,Encapsulation & Abstraction/Q6.cpp
+++ Constructor,Destructor,Encapsulation & Abstraction/Q6.cpp
@@ -3,4 +3,56 @@ private member variables for name, employee ID, and salary. Include
 member functions to calculate and set salary based on employee
 performance. Using of constructor */
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee {
+private:
+    string name;
+    int employeeID;
+    double salary;
+
+public:
+    Employee(string empName, int empID, double baseSalary) {
+        name = empName;
+        employeeID = empID;
+        salary = baseSalary;
+    }
+
+    void setSalary(double performanceFactor) {
+        salary += salary * performanceFactor;
+    }
+
+    void displayDetails() {
+        cout << "Employee Name: " << name << endl;
+        cout << "Employee ID: " << employeeID << endl;
+        cout << "Employee Salary: " << salary << endl;
+    }
+};
+
+int main() {
+    string name;
+    int id;
+    double baseSalary, performanceFactor;
+
+    cout << "Enter employee name: ";
+    cin >> name;
+    cout << "Enter employee ID: ";
+    cin >> id;
+    cout << "Enter base salary: ";
+    cin >> baseSalary;
+
+    Employee emp(name, id, baseSalary);
+
+    cout << "Enter performance factor (e.g., 0.10 for 10% increase): ";
+    cin >> performanceFactor;
+
+    emp.setSalary(performanceFactor);
+    emp.displayDetails();
+
+    return 0;
+}
+
+
 
